e6e83515a353903bb599883f6e326a4a1d41de24
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2026-02-14 14:13:57 +0800

    feat: add rv64 instrs

--- src/isa/riscv32/difftest/dut.c
+++ src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,20 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  extern CPU_state cpu;
+  for (int i = 0; i < 32; i++) {
+    if (cpu.gpr[i] != ref_r->gpr[i]) {
+      printf("DiffTest FAIL at pc = 0x%016lx\n", pc);
+      printf("reg[%d](%s) NEMU = 0x%016lx, REF = 0x%016lx\n", i, reg_name(i), cpu.gpr[i], ref_r->gpr[i]);
+      return false;
+    }
+  }
+  if (cpu.pc != ref_r->pc) {
+    printf("DiffTest FAIL at pc = 0x%016lx\n", pc);
+    printf("pc NEMU = 0x%016lx, REF = 0x%016lx\n", cpu.pc, ref_r->pc);
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
--- src/isa/riscv32/reg.c
+++ src/isa/riscv32/reg.c
@@ -24,8 +24,18 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  for (int i = 0; i < 32; i++) {
+    printf("%3s: 0x%016lx\n", regs[i], gpr(i));
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  for (int i = 0; i < 32; i++) {
+    if (strcmp(s, regs[i]) == 0) {
+      if (success) *success = true;
+      return gpr(i);
+    }
+  }
+  if (success) *success = false;
   return 0;
 }
